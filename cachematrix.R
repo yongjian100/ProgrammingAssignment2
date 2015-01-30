33808cd180ca6bff5be944881e652b73e0adfd51
yongjian100 <yongjian100@users.noreply.github.com>
2015-01-30 15:08:15 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -31,9 +31,3 @@ cacheSolve <- function(x=matrix(), ...) {
 }
 
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
